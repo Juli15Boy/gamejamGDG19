a7e33fe88beda430a70c5faf3f18436beb80e933
Silvia <silviagf94@gmail.com>
2019-11-23 17:19:09 +0100

    Crear escena MainMenu

---
+++ GameJam2019/Assets/Scripts/mainController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainController : MonoBehaviour
+{
+    public void ChangeScene(string nombre)
+    {
+        print("Cambiando a las escena" + nombre);
+    }
+}
