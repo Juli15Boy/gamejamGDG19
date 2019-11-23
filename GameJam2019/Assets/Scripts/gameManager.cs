27138b3a788add53fa6749fdc29b799720d5d0a7
Silvia <silviagf94@gmail.com>
2019-11-23 13:53:42 +0100

    Game Manager afegit

---
+++ GameJam2019/Assets/Scripts/gameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    [SerializeField] Transform spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void killPlayer()
+    {
+        transform.position = spawnPoint.position;
+        return;
+    }
+}
